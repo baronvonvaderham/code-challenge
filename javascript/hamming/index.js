2e8c51145e77a54c2e9e961376484d33b9f89499
Scott Johnson <baronvonvaderham@gmail.com>
2018-01-08 11:29:22 -0500

    Code Challenge Solutions
    Solutions in both Python and Javascript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -4,4 +4,19 @@
 // You may use ES6 or ES5 to solve.
 
 
+function Hamming (sequence1, sequence2) {
+    this.compute = function(sequence1, sequence2) {
+        if (sequence1.length != sequence2.length) {
+            throw("DNA strands must be of equal length.")
+        }
+        var diff = 0;
+        for (i = 0; i < sequence1.length; i++) {
+          if (sequence1[i] != sequence2[i]) {
+            diff = diff + 1;
+          }
+        }
+        return diff;
+    }
+}
+
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,12 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+
+def compute(first, second):
+    """
+    Function for calculating the hamming distance between
+    two nucleotide sequences, 'first' and 'second'
+    """
+    if len(first) != len(second):
+        raise ValueError
+    return sum(char1 != char2 for char1, char2 in zip(first, second))
